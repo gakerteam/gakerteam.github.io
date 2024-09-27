50d6f802b91dace8b42d94abff3de6b47ff8711f
gakerteam <gakerteam@users.noreply.github.com>
2024-09-27 02:03:28 +0000

    deploy: 2fc0cc31bee823de1db46dcbe5695276d2eb03ad

---
+++ assets/reference_developer_gaker-paas-api_dict.md.Cy-TsN18.js
@@ -0,0 +1,41 @@
+import{_ as i,c as a,a1 as n,o as t}from"./chunks/framework.C53VGXKD.js";const d=JSON.parse('{"title":"数据字典","description":"","frontmatter":{},"headers":[],"relativePath":"reference/developer/gaker-paas-api/dict.md","filePath":"reference/developer/gaker-paas-api/dict.md"}'),p={name:"reference/developer/gaker-paas-api/dict.md"};function e(l,s,h,o,k,r){return t(),a("div",null,s[0]||(s[0]=[n(`<h1 id="数据字典" tabindex="-1">数据字典 <a class="header-anchor" href="#数据字典" aria-label="Permalink to &quot;数据字典&quot;">​</a></h1><h3 id="获取国家区号" tabindex="-1">获取国家区号 <a class="header-anchor" href="#获取国家区号" aria-label="Permalink to &quot;获取国家区号&quot;">​</a></h3><p>请求接口：<code>/v1/open/base/countryCode</code></p><p>请求方法：<code>GET</code></p><p>请求参数：无</p><p>响应数据：</p><div class="language-json vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">json</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">{</span></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">    &quot;code&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">: </span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;0&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">,</span></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">    &quot;data&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">: [</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        {</span></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">            &quot;countryCode&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">: </span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;+86&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">,</span></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">            &quot;countryName&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">: </span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;中国大陆&quot;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        },</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        {</span></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">            &quot;countryCode&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">: </span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;+886&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">,</span></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">            &quot;countryName&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">: </span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;中国台湾&quot;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        },</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        {</span></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">            &quot;countryCode&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">: </span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;+852&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">,</span></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">            &quot;countryName&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">: </span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;中国香港&quot;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        },</span></span>
+<span class="line"><span style="--shiki-light:#B31D28;--shiki-light-font-style:italic;--shiki-dark:#FDAEB7;--shiki-dark-font-style:italic;">        ......</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        {</span></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">            &quot;countryCode&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">: </span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;+236&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">,</span></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">            &quot;countryName&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">: </span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;中非共和国&quot;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">        }</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">    ],</span></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">    &quot;msg&quot;</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">: </span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">&quot;success&quot;</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">}</span></span></code></pre></div><h3 id="获取行政区划" tabindex="-1">获取行政区划 <a class="header-anchor" href="#获取行政区划" aria-label="Permalink to &quot;获取行政区划&quot;">​</a></h3><p>请求接口：<code>/v1/open/base/administrativeDivision</code></p><p>请求方法：<code>GET</code></p><p>请求参数：</p><div class="language-JSON vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">JSON</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">目前仅支持查询中国的行政区划</span></span></code></pre></div><p>响应数据：返回数据是树形结构，比如下面展示的是四川省的行政区划</p><pre class="language-json"><code class="lang-json">{
+<strong>    &quot;code&quot;:&quot;0&quot;,
+</strong><strong>    &quot;data&quot;:[
+</strong>        {
+<strong>            &quot;cityList&quot;:Array[22],
+</strong><strong>            &quot;id&quot;:&quot;2284&quot;,
+</strong><strong>            &quot;pid&quot;:&quot;2283&quot;,
+</strong><strong>            &quot;value&quot;:&quot;成都市&quot;
+</strong>        },
+        {
+<strong>            &quot;cityList&quot;:Array[6],
+</strong><strong>            &quot;id&quot;:&quot;2304&quot;,
+</strong><strong>            &quot;pid&quot;:&quot;2283&quot;,
+</strong><strong>            &quot;value&quot;:&quot;自贡市&quot;
+</strong>        }
+    ],
+    &quot;msg&quot;: &quot;success&quot;
+}
+</code></pre>`,14)]))}const u=i(p,[["render",e]]);export{d as __pageData,u as default};
